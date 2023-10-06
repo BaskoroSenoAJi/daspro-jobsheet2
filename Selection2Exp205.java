e1dc6ffd5668f35e780a59eb4a38d609f9de7f9f
Baskoro Seno Aji <baskorosenoaji@gmail.com>
2023-10-06 13:28:21 +0700

    identify another triangle

--- Selection2Exp205.java
+++ Selection2Exp205.java
@@ -18,12 +18,18 @@ public class Selection2Exp205 {
         totalAngle = angle1 + angle2 + angle3;
 
         if(totalAngle == 180 )
-         if(angle1 == 90 || angle2 == 90 || angle3 == 90)
-            System.out.println("Right Triangle ");
+            //System.out.println("Right Triangle ");
+
+          if ( angle1 == angle2 && angle2 == angle3 )
+            System.out.println(" Equilateral Triangle ");
+        
+          else if ( angle1 == angle2 || angle1 == angle3 || angle2 == angle3)
+            System.out.println(" Isosceles Triangle");
+
         else 
-            System.out.println("Not a right Triangle");  
-        else 
-            System.out.println(" Not a triangle ");
+         System.out.println("Not even a Triangle ");
+        
+            
 
 
     input05.close();
