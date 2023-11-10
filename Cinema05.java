dee02b02df2db2d2f13698791e870743aa7b2953
Baskoro Seno Aji <baskorosenoaji@gmail.com>
2023-11-10 14:15:20 +0700

    adding for loop to display the length of each row

--- Cinema05.java
+++ Cinema05.java
@@ -20,7 +20,7 @@ public class Cinema05 {
         System.out.printf("%s \t %s \n", audience [3][0], audience [3][1]);
         */
 
-        // modify code sesuai soal 3 dan 4 
+        /*  modify code base on question 3 and 4
         audience [0][0] = "Amin" ;
         audience [0][1] = "Bena" ;
         audience [1][0] = "Candra" ; 
@@ -34,6 +34,38 @@ public class Cinema05 {
         System.out.println(audience[0].length);
         System.out.println(audience[1].length);
         System.out.println(audience[2].length);
-        System.out.println(audience[3].length);
+        System.out.println(audience[3].length); 
+        */
+
+        /*  modify the code base on question 5
+        audience [0][0] = "Amin" ;
+        audience [0][1] = "Bena" ;
+        audience [1][0] = "Candra" ; 
+        audience [1][1] = "Dela" ;
+        audience [2][0] = "Eka" ;
+        audience [2][1] = "Farhan" ;
+        audience [3][0] = "Gisel" ;
+        audience [3][1] = "Hana" ;
+
+        System.out.println(audience.length);
+        for ( int i = 0; i < audience.length; i++){
+            System.out.println(" Length of row " + ( i + 1 ) + " : " + audience[i].length);
+        }
+        */
+
+        // modify code base on question number 6 
+        audience [0][0] = "Amin" ;
+        audience [0][1] = "Bena" ;
+        audience [1][0] = "Candra" ; 
+        audience [1][1] = "Dela" ;
+        audience [2][0] = "Eka" ;
+        audience [2][1] = "Farhan" ;
+        audience [3][0] = "Gisel" ;
+        audience [3][1] = "Hana" ;
+
+        System.out.println(audience.length);
+        for ( int i = 0; i < audience.length; i++){
+            System.out.println(" Length of row " + ( i + 1 ) + " : " + audience[i].length);
+        }
     }
 }
