2cabddf646de11ae59ed048da17d83e135d4e399
Baskoro Seno Aji <baskorosenoaji@gmail.com>
2023-11-10 14:37:05 +0700

    modify the code by using for each loop to display all audience name

--- Cinema05.java
+++ Cinema05.java
@@ -110,5 +110,24 @@ public class Cinema05 {
             System.out.println(i);
         }
         */
+
+        // modify code base on question 12 
+        audience [0][0] = "Amin" ;
+        audience [0][1] = "Bena" ;
+        audience [1][0] = "Candra" ; 
+        audience [1][1] = "Dela" ;
+        audience [2][0] = "Eka" ;
+        audience [2][1] = "Farhan" ;
+        audience [3][0] = "Gisel" ;
+        audience [3][1] = "Hana" ;
+
+        System.out.println(audience.length);
+        for ( String [] rowAudience : audience ){
+            System.out.println(" Length of row " + rowAudience.length);
+        }
+
+        for ( int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row : " + ( i+1 ) + " = " +  String.join (", ", audience[i]));
+        }
     }
 }
